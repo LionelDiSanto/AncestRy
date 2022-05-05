beda9967b0b5e17f3c194f6c2c7338edf65ade0f
LionelDiSanto <lionel.disanto@ndsu.edu>
2022-05-05 10:01:43 +0200

    Adjustments

--- R/AncestRy_functions.R
+++ R/AncestRy_functions.R
@@ -12,8 +12,6 @@ NULL
 NULL
 #' @import LaplacesDemon
 NULL
-#' @import remotes
-NULL
 #'
 #' @title  Create Historical Events
 #' @description  This function generates the \code{events} data frame needed to run \code{\link{evolve2.0}} and \code{\link{evolve2.1}}.
